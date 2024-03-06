051572da2bee72b5da3ab714a57dc0ffe9b1e7f7
Greg <grzegorz.sokalski@optifly.com>
2024-03-06 19:09:42 +0100

    getting data and details for swapi game

--- src/app/shared/models/resources.model.ts
+++ src/app/shared/models/resources.model.ts
@@ -1,9 +1,9 @@
-import {People} from "./people.model";
+import {Person} from "./person.model";
 import {StarShip} from "./starship.model";
 
 export type ApiResponse<DataType> = {
   results: DataType
 }
 
-export type PeopleResponse = ApiResponse<People[]>
+export type PeopleResponse = ApiResponse<Person[]>
 export type StarshipResponse = ApiResponse<StarShip[]>
